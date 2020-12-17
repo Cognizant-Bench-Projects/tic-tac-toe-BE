ce8c28a38603bea5b8bbeb16956787950375fede
Jia Li <2001989@cognizant.com>
2020-12-17 01:08:29 -0500

    change to aws rds

--- src/test/java/com/cognizant/tictactoe/user/controller/UserControllerTest.java
+++ src/test/java/com/cognizant/tictactoe/user/controller/UserControllerTest.java
@@ -3,8 +3,6 @@ package com.cognizant.tictactoe.user.controller;
 import com.cognizant.tictactoe.user.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
